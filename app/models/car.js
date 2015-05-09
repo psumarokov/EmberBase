912b4fbec84b28970e9e53ebb44a1920295fbcea
psumarokov <pavel.sumarokov@aldigit.com>
2015-05-09 10:53:33 +0600

    Use link-to helpers

--- app/models/car.js
+++ app/models/car.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    img_host: 'http://localhost:3000',
     brand: DS.attr('String'),
     model: DS.attr('String'),
     image: DS.attr('String'),
-    _id: DS.attr('String')
+    _id: DS.attr('String'),
+    img_path: function() {
+        return 'http://localhost:3000' + this.get('image');
+    }.property('image'),
+    full_name: function() {
+        return this.get('brand') + ' ' + this.get('model');
+    }.property('brand', 'model')
 });
